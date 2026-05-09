e1199694eccff1578d38cffc217fb2dfefe05141
radioshomal <palmaweb.ir@gmail.com>
2026-05-09 17:54:43 +0330

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,22 @@ function updateClock(){
 
 updateClock();
 setInterval(updateClock,10000);
+
+function load(){
+
+const data = JSON.parse(localStorage.getItem("broadcast_pro"));
+if(!data) return;
+
+document.getElementById("overlay").style.width = data.width+"px";
+document.getElementById("overlay").style.height = data.height+"px";
+
+document.getElementById("title").innerText = data.title;
+document.getElementById("subtitle").innerText = data.subtitle;
+
+document.getElementById("overlay").style.background = data.bgColor;
+
+document.getElementById("title").style.color = data.titleColor;
+document.getElementById("subtitle").style.color = data.subtitleColor;
+}
+
+setInterval(load, 300);
