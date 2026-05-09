5e1f51bc938dcf6c170770d581e88dd0fd33105a
radioshomal <palmaweb.ir@gmail.com>
2026-05-09 17:40:01 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,73 +1,27 @@
 const clocks = [
-  {name:"IRAN",zone:"Asia/Tehran"},
-  {name:"ISRAEL",zone:"Asia/Jerusalem"},
-  {name:"EUROPE",zone:"Europe/Berlin"},
-  {name:"USA",zone:"America/New_York"}
+ {name:"IRAN", zone:"Asia/Tehran"},
+ {name:"ISRAEL", zone:"Asia/Jerusalem"},
+ {name:"EUROPE", zone:"Europe/Berlin"},
+ {name:"USA", zone:"America/New_York"}
 ];
 
-let clockIndex = 0;
+let i=0;
 
 function updateClock(){
 
-let c = clocks[clockIndex];
+ let c=clocks[i];
 
-let time = new Date().toLocaleTimeString(
-'en-US',
-{
-timeZone:c.zone
-}
-);
+ let time=new Date().toLocaleTimeString(
+   "en-US",
+   {timeZone:c.zone}
+ );
 
-document.getElementById("countryName").innerText = c.name;
-document.getElementById("countryTime").innerText = time;
+ document.getElementById("countryName").innerText=c.name;
+ document.getElementById("countryTime").innerText=time;
 
-clockIndex++;
-
-if(clockIndex >= clocks.length){
-clockIndex = 0;
+ i++;
+ if(i>=clocks.length) i=0;
 }
 
-}
-
-setInterval(updateClock,10000);
 updateClock();
-
-
-
-function loadData(){
-
-let data = JSON.parse(
-localStorage.getItem("broadcastData")
-);
-
-if(!data) return;
-
-
-let box = document.getElementById("lowerThird");
-
-box.style.width = data.width + "px";
-box.style.height = data.height + "px";
-
-box.style.background = data.bgColor;
-
-document.getElementById("titleText").innerText = data.title;
-
-document.getElementById("subtitleText").innerText = data.subtitle;
-
-document.getElementById("titleText").style.color = data.titleColor;
-
-document.getElementById("subtitleText").style.color = data.subtitleColor;
-
-document.getElementById("titleText").style.fontSize =
-data.titleSize + "px";
-
-document.getElementById("subtitleText").style.fontSize =
-data.subtitleSize + "px";
-
-document.getElementById("logo").src = data.logo;
-
-}
-
-
-setInterval(loadData,500);
-loadData();
+setInterval(updateClock,10000);
