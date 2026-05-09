a4df65813b4d73788787e2ea43acceba4a28e32c
radioshomal <palmaweb.ir@gmail.com>
2026-05-09 17:06:56 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,59 @@
-const clocks = [
- {name:"IRAN", zone:"Asia/Tehran"},
- {name:"ISRAEL", zone:"Asia/Jerusalem"},
- {name:"EUROPE", zone:"Europe/Berlin"},
- {name:"USA", zone:"America/New_York"}
+const zones = [
+{label:"IRAN", zone:"Asia/Tehran"},
+{label:"ISRAEL", zone:"Asia/Jerusalem"},
+{label:"EUROPE", zone:"Europe/Berlin"},
+{label:"USA", zone:"America/New_York"}
 ];
 
-let i=0;
+let current = 0;
 
 function updateClock(){
+const z = zones[current];
 
- let c=clocks[i];
+const time = new Date().toLocaleTimeString("en-US",{
+timeZone:z.zone
+});
 
- let time=new Date().toLocaleTimeString(
-   "en-US",
-   {timeZone:c.zone}
- );
+document.getElementById("clock").innerHTML=
+`${z.label}<br>${time}`;
 
- document.getElementById("countryName").innerText=c.name;
- document.getElementById("countryTime").innerText=time;
-
- i++;
- if(i>=clocks.length) i=0;
+current=(current+1)%zones.length;
 }
 
 updateClock();
 setInterval(updateClock,10000);
+
+window.addEventListener("storage",()=>{
+
+const data=JSON.parse(localStorage.getItem("newsData"));
+
+if(!data) return;
+
+const box=document.getElementById("newsBox");
+const title=document.getElementById("title");
+const desc=document.getElementById("desc");
+
+box.style.width=data.width+"px";
+box.style.height=data.height+"px";
+box.style.background=data.bg;
+
+title.innerText=data.title;
+desc.innerText=data.desc;
+
+title.style.color=data.titleColor;
+desc.style.color=data.descColor;
+
+title.style.fontSize=data.titleSize+"px";
+desc.style.fontSize=data.descSize+"px";
+
+title.style.fontFamily=data.font;
+desc.style.fontFamily=data.font;
+
+document.getElementById("logo").src=data.logo;
+
+title.className=data.effect;
+desc.className=data.effect;
+
+});
+
+dispatchEvent(new Event("storage"));
