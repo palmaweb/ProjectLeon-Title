eb7e6fb5803601fc85aefa61237f90ef25bb03f5
radioshomal <palmaweb.ir@gmail.com>
2026-05-09 17:56:00 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-const clocks = [
- {name:"IRAN", zone:"Asia/Tehran"},
- {name:"ISRAEL", zone:"Asia/Jerusalem"},
- {name:"EUROPE", zone:"Europe/Berlin"},
- {name:"USA", zone:"America/New_York"}
-];
-
-let i=0;
-
-function updateClock(){
-
- let c=clocks[i];
-
- let time=new Date().toLocaleTimeString(
-   "en-US",
-   {timeZone:c.zone}
- );
-
- document.getElementById("countryName").innerText=c.name;
- document.getElementById("countryTime").innerText=time;
-
- i++;
- if(i>=clocks.length) i=0;
-}
-
-updateClock();
-setInterval(updateClock,10000);
-
 function load(){
 
 const data = JSON.parse(localStorage.getItem("broadcast_pro"));
