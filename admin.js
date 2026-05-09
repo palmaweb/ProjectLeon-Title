30b566775b1f91b9835c59b1f8037abe50b2f3c8
radioshomal <palmaweb.ir@gmail.com>
2026-05-09 17:08:09 +0330

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,72 +1,26 @@
-function saveData(){
+function save(){
 
-    const data = {
+const data={
 
-        title:
-        document.getElementById("title").value,
+width:width.value,
+height:height.value,
+bg:bg.value,
+logo:logo.value,
 
-        subtitle:
-        document.getElementById("subtitle").value,
+title:title.value,
+desc:desc.value,
 
-        logo:
-        document.getElementById("logo").value,
+titleColor:titleColor.value,
+descColor:descColor.value,
 
-        bg:
-        document.getElementById("bg").value,
+titleSize:titleSize.value,
+descSize:descSize.value,
 
-        leftColor:
-        document.getElementById("leftColor").value,
+font:font.value,
+effect:effect.value
 
-        titleColor:
-        document.getElementById("titleColor").value,
+};
 
-        subtitleColor:
-        document.getElementById("subtitleColor").value,
+localStorage.setItem("newsData",JSON.stringify(data));
 
-        live:
-        document.getElementById("live").checked
-    };
-
-    localStorage.setItem(
-        "overlay",
-        JSON.stringify(data)
-    );
-
-    alert("ذخیره شد");
 }
-
-function loadData(){
-
-    const data =
-    JSON.parse(
-        localStorage.getItem("overlay")
-    );
-
-    if(!data) return;
-
-    document.getElementById("title").value =
-    data.title;
-
-    document.getElementById("subtitle").value =
-    data.subtitle;
-
-    document.getElementById("logo").value =
-    data.logo;
-
-    document.getElementById("bg").value =
-    data.bg;
-
-    document.getElementById("leftColor").value =
-    data.leftColor;
-
-    document.getElementById("titleColor").value =
-    data.titleColor;
-
-    document.getElementById("subtitleColor").value =
-    data.subtitleColor;
-
-    document.getElementById("live").checked =
-    data.live;
-}
-
-loadData();
