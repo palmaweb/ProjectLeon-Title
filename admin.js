c6eeb7729df74738f1b901d653270f8d561c53cc
radioshomal <palmaweb.ir@gmail.com>
2026-05-09 14:46:48 +0330

    Add leftColor storage in saveData function

--- admin.js
+++ admin.js
@@ -11,6 +11,39 @@ function saveData(){
     );
 
     localStorage.setItem(
+        "logo",
+        document.getElementById("logo").value
+    );
+
+    localStorage.setItem(
+        "panelColor",
+        document.getElementById("panelColor").value
+    );
+
+    localStorage.setItem(
+        "titleColor",
+        document.getElementById("titleColor").value
+    );
+
+    localStorage.setItem(
+        "subtitleColor",
+        document.getElementById("subtitleColor").value
+    );
+
+    alert("ذخیره شد");
+}function saveData(){
+
+    localStorage.setItem(
+        "title",
+        document.getElementById("title").value
+    );
+
+    localStorage.setItem(
+        "subtitle",
+        document.getElementById("subtitle").value
+    );
+
+    localStorage.setItem(
         "panelColor",
         document.getElementById("panelColor").value
     );
