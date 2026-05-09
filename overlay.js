406a9d1e01db33b5d05fa91e97ea29058ea5d812
radioshomal <palmaweb.ir@gmail.com>
2026-05-09 14:46:12 +0330

    Refactor variables to use const in loadData function

--- overlay.js
+++ overlay.js
@@ -1,25 +1,42 @@
 function loadData(){
 
-    let title = localStorage.getItem("title");
-    let subtitle = localStorage.getItem("subtitle");
+    const title = localStorage.getItem("title");
+    const subtitle = localStorage.getItem("subtitle");
 
-    let panelColor = localStorage.getItem("panelColor");
-    let leftColor = localStorage.getItem("leftColor");
+    const panelColor = localStorage.getItem("panelColor");
+    const titleColor = localStorage.getItem("titleColor");
+    const subtitleColor = localStorage.getItem("subtitleColor");
+
+    const logo = localStorage.getItem("logo");
 
     if(title){
+
         document.getElementById("title").innerText = title;
     }
 
     if(subtitle){
+
         document.getElementById("subtitle").innerText = subtitle;
     }
 
     if(panelColor){
+
         document.getElementById("news-box").style.background = panelColor;
     }
 
-    if(leftColor){
-        document.getElementById("left-box").style.background = leftColor;
+    if(titleColor){
+
+        document.getElementById("title").style.color = titleColor;
+    }
+
+    if(subtitleColor){
+
+        document.getElementById("subtitle").style.color = subtitleColor;
+    }
+
+    if(logo){
+
+        document.getElementById("logo").src = logo;
     }
 
 }
